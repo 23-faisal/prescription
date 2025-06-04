4cabb51a418164b87b4a493e0ba65431d98d16dd
23-faisal <a.faisal23r@gmail.com>
2025-06-04 08:33:34 +0600

    create speciality menu

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/common/Hero";
+import SpecialityMenu from "@/components/common/SpecialityMenu";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <SpecialityMenu />
     </div>
   );
 };
